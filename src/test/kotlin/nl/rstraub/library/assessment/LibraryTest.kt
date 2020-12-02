515bff396bf9d73836ad1002f61fd944376cdc14
Roy Straub <roy.straub@codesquad.nl>
2020-12-02 19:45:53 +0100

    Using wordspec to group tests more elegantly

--- src/test/kotlin/nl/rstraub/library/assessment/LibraryTest.kt
+++ src/test/kotlin/nl/rstraub/library/assessment/LibraryTest.kt
@@ -1,20 +1,22 @@
 package nl.rstraub.library.assessment
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 
-internal class LibraryTest : StringSpec({
-    "inventory should return empty list given zero books" {
-        Library().inventory shouldBe emptyList()
-    }
+internal class LibraryTest : WordSpec({
+    "inventory" should {
+        "return empty list given zero books" {
+            Library().inventory shouldBe emptyList()
+        }
 
-    "inventory should return all books in the library" {
-        val library = Library("moby dick", "picture of dorian gray")
+        "return all books in the library" {
+            val library = Library("moby dick", "picture of dorian gray")
 
-        val result = library.inventory
+            val result = library.inventory
 
-        result shouldContain "moby dick"
-        result shouldContain "picture of dorian gray"
+            result shouldContain "moby dick"
+            result shouldContain "picture of dorian gray"
+        }
     }
 })
